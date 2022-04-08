3e8007156bf5e7dde0fc9389c39f2f363ff23b51
meghana <meghana.hegde@niveussolutions.com>
2022-04-08 11:02:30 +0530

    src/pages/Viewproduct.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login/index";
 import Dashboard from "../pages/Dashboard/index";
 import ViewDetails from "../pages/Details/index";
+import Viewproduct from "../pages/Viewproduct.js";
 
 const MainRoutes = ({ isAuthenticated = true }) => {
   return (
@@ -11,6 +12,7 @@ const MainRoutes = ({ isAuthenticated = true }) => {
       <Route path="/login" element={<Login />} />
       <Route exact path="/dashboard" element={<Dashboard />} />
       <Route exact path="/view" element={<ViewDetails />} />
+      <Route path="/view" element={<Viewproduct />} />
 
     </Routes>
   );
