b7d88a02cef12dd9c64662da57081a5bfefdbe50
meghana <meghana.hegde@niveussolutions.com>
2022-07-11 22:49:48 +0530

    initial commit

--- src/pages/vehical/Sidebar.js
+++ src/pages/vehical/Sidebar.js
@@ -1,27 +1,56 @@
 import React from "react";
-import Car from "../../assets/Vector car.png";
-import Logout from "../../assets/logout.png";
-import Product from "../../assets/product.png";
- 
+import Profile from "../assets/profile.png";
+import Product from "../assets/product.png";
+import Car from "../assets/Vector car.png";
+import Masters from "../assets/setting.png" ;
+import { Link, useLocation } from "react-router-dom";
+
 const SidebarData = [
-  { name: "Products" , image: Product },
-  { name: "Vehicle Mapping" , image: Car},
-  
+  { name: "Products", image: Product, pathName: "/dashboard" },
+  { name: "Vehicle Mapping", image: Car, pathName: "/vehicle" },
+  { name: "Masters", image: Masters, pathName: "/masters" },
 ];
 
-const Sidebar = ({ width, height }) => {
+const Sidebar = ({style}) => {
+  const location = useLocation();
   return (
-    <div style={{ width: "20%", backgroundColor: "#1E4597", height: height - 70 , padding: "20px 0px" , boxSizing: "border-box" , display: "flex" , alignItems: "center" , flexDirection:"column"}}>
-       <div>
+    <div
+      style={{
+        width: "20%",
+        backgroundColor: "#1E4597",
+        //minHeight: window.innerHeight - 70 ,
+        padding: "20px 0px",
+        boxSizing: "border-box",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        ...style
+        
+      }}
+    >
+      <div>
         {SidebarData.map((item, i) => {
           return (
-            <div style={{display: "flex" , marginTop:"30px"  , color: "white"}} key={i} >
-            <img src={item.image} style={{marginRight:15 , objectFit:"contain"}}/>
-            <p style={{margin:0}}>{item.name}</p>
-            </div>
+            <Link
+              to={item?.pathName}
+              style={{
+                display: "flex",
+                marginTop: "30px",
+                color: "white",
+                opacity: location.pathname === item.pathName ? 1 : 0.5,
+                textDecoration: "none",
+              }}
+              key={i}
+            >
+              <img
+                src={item.image}
+                style={{ marginRight: 15, objectFit: "contain" }}
+              />
+              <p style={{ margin: 0 }}>{item.name}</p>
+            </Link>
           );
         })}
-  </div>
+      </div>
     </div>
   );
 };
